418929e10a6ee921c5a378300c1e97411ce11d89
Peter Cortez <innov.petercortez@gmail.com>
2024-06-24 16:55:40 +0800

    LO-001: Add line number when logging bad log entries

--- src/Service/LogFileImporter/LogFileImporter.php
+++ src/Service/LogFileImporter/LogFileImporter.php
@@ -5,6 +5,7 @@ namespace App\Service\LogFileImporter;
 use App\Dto\Entity\LogEntry\Assembler\FromString;
 use App\Util\File\Support\Contracts;
 use Psr\Log\LoggerInterface;
+use SplFileObject;
 
 /**
  * @package App\Service\LogFileImporter
@@ -29,14 +30,14 @@ class LogFileImporter implements Support\Contracts\LogFileImporterInterface
     {
         $iterator->seek($offset);
         $iterator->limit($limit);
-        $iterator->chunk($chunk_size);
+        $iterator->chunk($chunk_size, fn (SplFileObject $file) => [$file->key() + 1, $file->fgets()]);
 
         while ($iterator->valid()) {
             $importable_log_entry_dtos = [];
 
             // Because we are chunking the lines that are being read from the file, each iteration using a loop will
             // yield an array of lines, instead of a string comprising a single line.
-            foreach ($iterator->current() as $line) {
+            foreach ($iterator->current() as [$line_number, $line]) {
                 $assembler = new FromString($line);
                 $log_entry_dto = $assembler->assemble();
 
@@ -44,7 +45,12 @@ class LogFileImporter implements Support\Contracts\LogFileImporterInterface
                 // Thus, we can skip it.
                 if (empty($log_entry_dto)) {
                     // TODO: Update the test here to add an assertion on the line number that's being skipped.
-                    $this->logger->warning('Skipping log entry with mismatched format', ['line' => $line]);
+                    $this
+                        ->logger
+                        ->warning('Skipping log entry with mismatched format', [
+                            'line' => $line_number,
+                            'content' => $line,
+                        ]);
 
                     continue;
                 }
